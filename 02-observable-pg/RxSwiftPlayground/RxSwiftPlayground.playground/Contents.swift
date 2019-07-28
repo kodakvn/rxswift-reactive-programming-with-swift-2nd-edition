d910b640a7a3f9b10bf79567ed3ac51f0b05fb72
KODAK <linhtt.it@gmail.com>
2019-07-28 21:12:19 +0700

    02-observables - Creating observable factories

--- 02-observable-pg/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ 02-observable-pg/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -62,52 +62,134 @@ import RxSwift
 
 // ### Disposing and terminating
 
-example(of: "dispose") {
+//example(of: "dispose") {
+//    // 1
+//    let observable = Observable.of("A", "B", "C")
+//
+//    // 2
+//    let subscription = observable.subscribe { event in
+//        // 3
+//        print(event)
+//    }
+//
+//    subscription.dispose()
+//}
+//
+//example(of: "DisposeBag") {
+//    // 1
+//    let disposeBag = DisposeBag()
+//
+//    // 2
+//    Observable.of("A", "B", "C")
+//        .subscribe {
+//            print($0)
+//        }
+//        .disposed(by: disposeBag)
+//}
+//
+//example(of: "create") {
+//    enum MyError: Error {
+//        case anError
+//    }
+//
+//    let disposeBag = DisposeBag()
+//
+//    Observable<String>.create { observer in
+//            observer.onNext("1")
+////            observer.onError(MyError.anError)
+////            observer.onCompleted()
+//            observer.onNext("?")
+//            return Disposables.create()
+//        }
+//        .subscribe(
+//            onNext: { print($0) },
+//            onError: { print($0) },
+//            onCompleted: { print("completed") },
+//            onDisposed: { print("disposed") }
+//        )
+////        .disposed(by: disposeBag)
+//}
+
+// ### Creating observable factories ###
+
+example(of: "deferred") {
+    let disposeBag = DisposeBag()
+    
     // 1
-    let observable = Observable.of("A", "B", "C")
+    var flip = false
     
     // 2
-    let subscription = observable.subscribe { event in
+    let factory: Observable<Int> = Observable.deferred {
         // 3
-        print(event)
+        flip = !flip
+        
+        // 4
+        if flip {
+            return Observable.of(1,2,3)
+        } else {
+            return Observable.of(4,5,6)
+        }
     }
     
-    subscription.dispose()
+    for _ in 0...3 {
+        factory.subscribe(onNext: {
+            print($0, terminator: "")
+        })
+        .disposed(by: disposeBag)
+        
+        print()
+    }
 }
 
-example(of: "DisposeBag") {
+example(of: "Single") {
     // 1
     let disposeBag = DisposeBag()
     
     // 2
-    Observable.of("A", "B", "C")
+    enum FileReadError: Error {
+        case fileNotFound, unreadable, encodingFailed
+    }
+    
+    // 3
+    func loadText(from name: String) -> Single<String> {
+        return Single.create { single in
+            // 1
+            let disposable = Disposables.create()
+            
+            // 2
+            guard let path = Bundle.main.path(forResource: name, ofType: "txt") else {
+                single(.error(FileReadError.fileNotFound))
+                return disposable
+            }
+            
+            // 3
+            guard let data = FileManager.default.contents(atPath: path) else {
+                single(.error(FileReadError.unreadable))
+                return disposable
+            }
+            
+            // 4
+            guard let contents = String(data: data, encoding: .utf8) else {
+                single(.error(FileReadError.encodingFailed))
+                return disposable
+            }
+            
+            // 5
+            single(.success(contents))
+            return disposable
+        }
+    }
+    
+    loadText(from: "Copyright")
         .subscribe {
-            print($0)
+            switch $0 {
+            case .success(let string):
+                print(string)
+            case .error(let error):
+                print(error)
+            }
         }
         .disposed(by: disposeBag)
-}
-
-example(of: "create") {
-    enum MyError: Error {
-        case anError
-    }
-    
-    let disposeBag = DisposeBag()
-    
-    Observable<String>.create { observer in
-            observer.onNext("1")
-//            observer.onError(MyError.anError)
-//            observer.onCompleted()
-            observer.onNext("?")
-            return Disposables.create()
-        }
-        .subscribe(
-            onNext: { print($0) },
-            onError: { print($0) },
-            onCompleted: { print("completed") },
-            onDisposed: { print("disposed") }
-        )
-//        .disposed(by: disposeBag)
 }
 /*:
  Copyright (c) 2014-2017 Razeware LLC
