3c4557b65218b416d87db35d2350f509b43e9703
KODAK <linhtt.it@gmail.com>
2019-11-03 23:05:58 +0700

    10-combining-operators-in-practice - generic request & fetch categories

--- 10-combining-operators-in-practice/OurPlanet/OurPlanet/CategoriesViewController.swift
+++ 10-combining-operators-in-practice/OurPlanet/OurPlanet/CategoriesViewController.swift
@@ -27,6 +27,8 @@ import RxCocoa
 class CategoriesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var tableView: UITableView!
+    let categories = Variable<[EOCategory]>([])
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,16 +37,33 @@ class CategoriesViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func startDownload() {
+        let eoCategories = EONET.categories
+        eoCategories
+            .bind(to: categories)
+            .disposed(by: disposeBag)
         
+        eoCategories
+            .asObservable()
+            .subscribe(onNext: { [weak self] _ in
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            })
+            .disposed(by: disposeBag)
     }
     
     // MARK: UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return categories.value.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell")!
+        
+        let category = categories.value[indexPath.row]
+        cell.textLabel?.text = category.name
+        cell.detailTextLabel?.text = category.description
+        
         return cell
     }
     
