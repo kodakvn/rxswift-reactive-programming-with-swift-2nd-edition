1eae6a0a59606242c5116e819d78dd050d25fccb
KODAK <linhtt.it@gmail.com>
2019-11-22 22:44:36 +0700

    15-intro-to-schedulers - create thread

--- 15-intro-to-schedulers/Schedulers/Schedulers/main.swift
+++ 15-intro-to-schedulers/Schedulers/Schedulers/main.swift
@@ -31,24 +31,41 @@ let animal = BehaviorSubject(value: "[dog]")
 
 
 animal
+    .subscribeOn(MainScheduler.instance)
     .dump()
+    .observeOn(globalScheduler)
     .dumpingSubscription()
     .disposed(by: bag)
 
-let fruit = Observable<String>.create { observer in
-    observer.onNext("[apple]")
-    sleep(2)
-    observer.onNext("[pineapple]")
-    sleep(2)
-    observer.onNext("[strawberry]")
-    return Disposables.create()
+//let fruit = Observable<String>.create { observer in
+//    observer.onNext("[apple]")
+//    sleep(2)
+//    observer.onNext("[pineapple]")
+//    sleep(2)
+//    observer.onNext("[strawberry]")
+//    return Disposables.create()
+//}
+
+//fruit
+//    .subscribeOn(globalScheduler)
+//    .dump()
+//    .observeOn(MainScheduler.instance)
+//    .dumpingSubscription()
+//    .disposed(by: bag)
+//
+
+let animalsThread = Thread() {
+    sleep(3)
+    animal.onNext("[cat]")
+    sleep(3)
+    animal.onNext("[tiger]")
+    sleep(3)
+    animal.onNext("[fox]")
+    sleep(3)
+    animal.onNext("[leopard]")
 }
 
-fruit
-    .subscribeOn(globalScheduler)
-    .dump()
-    .observeOn(MainScheduler.instance)
-    .dumpingSubscription()
-    .disposed(by: bag)
+animalsThread.name = "Animals thread"
+animalsThread.start()
 
 RunLoop.main.run(until: Date.init(timeIntervalSinceNow: 13))
